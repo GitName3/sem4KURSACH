07c26507edf43b051d0a51cf099d063c214af8d0
Vlasov Anton <CodingEnjoyer413@gmail.com>
2024-05-08 23:00:07 +0300

    last commit before adding bootstrap

--- Pizzeria/src/main/java/com/example/Pizzeria/configurations/SecurityConfig.java
+++ Pizzeria/src/main/java/com/example/Pizzeria/configurations/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.example.Pizzeria.configurations;
 
 import com.example.Pizzeria.services.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig{
     private final CustomUserDetailsService customUserDetailsService;
 
@@ -43,16 +47,15 @@ public class SecurityConfig{
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-//    @Bean
-//    public AuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(customUserDetailsService);
-//        authenticationProvider.setPasswordEncoder(passwordEncoder());
-//        return authenticationProvider;
-//    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(8);
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService);
+    }
+
 
 }
--- Pizzeria/src/main/java/com/example/Pizzeria/services/UserService.java
+++ Pizzeria/src/main/java/com/example/Pizzeria/services/UserService.java
@@ -9,6 +9,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -25,5 +28,34 @@ public class UserService {
         userRepository.save(user);
         log.info("User created: " + user);
         return true;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if(user != null) {
+            if (user.isActive()){
+                log.info("User with id={} and email={} banned: ", user.getId(), user.getEmail());
+                user.setActive(false);
+            } else {
+                user.setActive(true);
+                log.info("User with id={} and email={} unbanned: ", user.getId(), user.getEmail());
+            }
+            userRepository.save(user);
+        }
+    }
+
+    public void changeUserRoles(User user, Map<String, String> form) {
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()){
+            if (roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
     }
 }
